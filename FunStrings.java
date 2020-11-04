0eee1ffa1b8f4abba7b3015432ad9dea339a7719
Collin Holmquist <collinholmquist@gmail.com>
2020-11-04 15:12:47 -0600

    Create FunStrings.java

---
+++ FunStrings.java
@@ -0,0 +1,21 @@
+public class FunStrings {
+ 
+ public static String guessTheString(String guess){
+ 
+ 
+    return ""; 
+ }
+ 
+ public static void setHiddenWord(String hiddenWord) {
+ 
+ 
+ 
+ }
+ 
+ 
+ public static String removeLetter(String phrase, String letter) {
+ 
+    return ""; 
+ }
+ 
+}
